77cf7dafdef41d26a6b029cdeba737f533b77421
SFonderie <sydney3449@gmail.com>
2022-11-22 19:03:16 -0800

    Changes to Composite

--- Composite/Composite.h
+++ Composite/Composite.h
@@ -1,4 +1,36 @@
-#ifndef COMPOSITE_H
-#define COMPOSITE_H
+#pragma once
 
-#endif
+// Forward declaration.
+class UComposite;
+struct FName;
+
+// Reference-based float wrapper.
+// USTRUCT(BlueprintType)
+struct FCompositeValue
+{
+	// GENERATED_BODY()
+
+	// Derive the value from this composite.
+	// UPROPERTY()
+	UComposite* Reference = nullptr;
+
+	// Value if the reference is null.
+	// UPROPERTY()
+	float ConstantValue = 0;
+};
+
+// Denotes how the composite modifier applies to the composite.
+// UENUM()
+enum class ECompositeModifierType
+{
+	DirectAddition,
+	PercentAddition,
+	Multiplication,
+};
+
+// Reference-enabled float modifier.
+// USTRUCT(BlueprintType)
+struct FCompositeModifier : public FCompositeValue
+{
+	
+};
